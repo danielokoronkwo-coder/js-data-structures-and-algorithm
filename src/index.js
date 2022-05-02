1e7e7af502201508fffe52ccdf47f571b1185219
Daniel Okoronkwo <61119104+danielokoronkwo-coder@users.noreply.github.com>
2022-05-02 18:20:07 +0100

    Added Body Parser package

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 let express = require("express");
+const bodyParser = require("body-parser");
 let app = express();
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 const PORT = 8080;
 app.listen(PORT, () => {
-  console.log(`server is live and listening to ${PORT}`);
+	console.log(`server is live and listening to ${PORT}`);
 });
